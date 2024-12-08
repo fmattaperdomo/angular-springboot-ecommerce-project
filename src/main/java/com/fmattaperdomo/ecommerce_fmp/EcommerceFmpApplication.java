a7afe27640c6e6f58f919a3f375cc610d2ece807
fmattaperdomo <fmattaperdomo@gmail.com>
2024-12-08 10:38:42 -0500

    Initial Version

---
+++ src/main/java/com/fmattaperdomo/ecommerce_fmp/EcommerceFmpApplication.java
@@ -0,0 +1,13 @@
+package com.fmattaperdomo.ecommerce_fmp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceFmpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceFmpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fmattaperdomo/ecommerce_fmp/EcommerceFmpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fmattaperdomo.ecommerce_fmp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceFmpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
